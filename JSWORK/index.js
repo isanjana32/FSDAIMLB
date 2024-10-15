6c98cb52290056b4e57e04316aacb22472e76355
isanjana32 <isanjana32@gmail.com>
2024-10-15 10:28:39 +0530

    AMAZON

---
+++ JSWORK/index.js
@@ -0,0 +1,16 @@
+console.log("Hello JS");
+// const a=12;
+// if(a>10){
+//     let b=23;
+//     a=40;
+//     console.log("a="+a);
+// }
+// console.log("hi="+b)
+
+function fun(a,b){
+    console.log("heee");
+    return a+b;
+}
+
+let d=fun();
+console.log(d);
